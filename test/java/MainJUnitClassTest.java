d3e3f44bb80bcd672f7647be20c0909f13546c04
YashG0728 <yashgharde07@gmail.com>
2022-09-14 22:03:56 +0530

    Added : Mobile Format Using JUnit And Regex

--- test/java/MainJUnitClassTest.java
+++ test/java/MainJUnitClassTest.java
@@ -7,20 +7,26 @@ public class MainJUnitClassTest {
     @Test
     public void givenFirstCharacterUpperCase_whenTested_shouldReturnTrue() {
         MainJUnitClass firstName = new MainJUnitClass();
-        boolean result = firstName.checkFirstName("Yash");
+        boolean result = firstName.checkFirstName("Akash");
         assertTrue(result);
     }
 
     @Test
     public void givenFirstCharacterUpperCase_whenTestedLastName_shouldReturnTrue() {
         MainJUnitClass firstName = new MainJUnitClass();
-        boolean result = firstName.checkFirstName("Gharde");
+        boolean result = firstName.checkFirstName("Das");
         assertTrue(result);
     }
     @Test
     public void emailCheck_shouldReturnTrue() {
         MainJUnitClass email = new MainJUnitClass();
-        boolean result = email.checkEmail("yashgharde07@gmail.com");
+        boolean result = email.checkEmail("akash595@gmail.com");
+        assertTrue(result);
+    }
+    @Test
+    public void mobileNumberFormatCheck_shouldReturnTrue() {
+        MainJUnitClass mobileNumber = new MainJUnitClass();
+        boolean result = mobileNumber.checkMobileNumber("91 9910162995");
         assertTrue(result);
     }
 }
