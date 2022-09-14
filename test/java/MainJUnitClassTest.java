7378c01d3652c28792128e3830a2ecffe65718cd
YashG0728 <yashgharde07@gmail.com>
2022-09-14 21:53:06 +0530

    Added : Email Using JUnit And Regex

--- test/java/MainJUnitClassTest.java
+++ test/java/MainJUnitClassTest.java
@@ -1,11 +1,26 @@
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MainJUnitClassTest {
+
     @Test
     public void givenFirstCharacterUpperCase_whenTested_shouldReturnTrue() {
         MainJUnitClass firstName = new MainJUnitClass();
         boolean result = firstName.checkFirstName("Yash");
+        assertTrue(result);
+    }
+
+    @Test
+    public void givenFirstCharacterUpperCase_whenTestedLastName_shouldReturnTrue() {
+        MainJUnitClass firstName = new MainJUnitClass();
+        boolean result = firstName.checkFirstName("Gharde");
+        assertTrue(result);
+    }
+    @Test
+    public void emailCheck_shouldReturnTrue() {
+        MainJUnitClass email = new MainJUnitClass();
+        boolean result = email.checkEmail("yashgharde07@gmail.com");
         assertTrue(result);
     }
 }
