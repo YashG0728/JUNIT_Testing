00108aa026c4b60ceae54391e674702085525141
yash <107634548+YashG0728@users.noreply.github.com>
2022-09-15 20:21:03 +0530

    Update MainJUnitClassTest.java

--- test/java/MainJUnitClassTest.java
+++ test/java/MainJUnitClassTest.java
@@ -7,26 +7,26 @@ public class MainJUnitClassTest {
     @Test
     public void givenFirstCharacterUpperCase_whenTested_shouldReturnTrue() {
         MainJUnitClass firstName = new MainJUnitClass();
-        boolean result = firstName.checkFirstName("Akash");
+        boolean result = firstName.checkFirstName("Yash");
         assertTrue(result);
     }
 
     @Test
     public void givenFirstCharacterUpperCase_whenTestedLastName_shouldReturnTrue() {
         MainJUnitClass firstName = new MainJUnitClass();
-        boolean result = firstName.checkFirstName("Das");
+        boolean result = firstName.checkFirstName("Gharde");
         assertTrue(result);
     }
     @Test
     public void emailCheck_shouldReturnTrue() {
         MainJUnitClass email = new MainJUnitClass();
-        boolean result = email.checkEmail("akash595@gmail.com");
+        boolean result = email.checkEmail("yashgharde@gmail.com");
         assertTrue(result);
     }
     @Test
     public void mobileNumberFormatCheck_shouldReturnTrue() {
         MainJUnitClass mobileNumber = new MainJUnitClass();
-        boolean result = mobileNumber.checkMobileNumber("91 9910162995");
+        boolean result = mobileNumber.checkMobileNumber("91 9910313216");
         assertTrue(result);
     }
 }
